233413e53d9a04da6700e4b50162e74faab68c76
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-12-10 19:15:57 +0100

    FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { ObjectId } from 'mongodb';
 import fs from 'fs';
 import path from 'path';
+import mime from 'mime-types';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -214,6 +215,37 @@ class FilesController {
         isPublic: updatedFile.isPublic,
         parentId: updatedFile.parentId,
       });
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getFile(req, res) {
+    const token = req.headers['x-token'];
+    const { id } = req.params;
+
+    try {
+      const filesCollection = await dbClient.db.collection('files');
+      const file = await filesCollection.findOne({ _id: new ObjectId(id) });
+
+      if (!file) return res.status(404).json({ error: 'Not found' });
+
+      if (file.type === 'folder') return res.status(400).json({ error: "A folder doesn't have content" });
+
+      const userId = await redisClient.get(`auth_${token}`);
+
+      if (!file.isPublic && (!userId || userId !== file.userId.toString())) return res.status(404).json({ error: 'Not found' });
+
+      if (!fs.existsSync(file.localPath)) return res.status(404).json({ error: 'Not found' });
+
+      const mimeType = mime.lookup(file.name) || 'application/octet-stream';
+
+      res.setHeader('Content-Type', mimeType);
+
+      const fileContent = fs.createReadStream(file.localPath);
+
+      fileContent.pipe(res);
     } catch (error) {
       console.error(error.message);
       return res.status(500).json({ error: 'Internal Server Error' });
