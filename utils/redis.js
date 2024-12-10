3b00ac9b9a40e9a536b8504e2176e93b1cb59ff8
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-12-10 16:38:15 +0100

    task0

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+
+        this.client.on('error', (err) => {
+            console.log(err.message);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        const GET = promisify(this.client.get).bind(this.client);
+        return GET(key);
+    }
+
+    async set(key, value, duration) {
+        const SET = promisify(this.client.set).bind(this.client);
+        return SET(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        const DEL = promisify(this.client.del).bind(this.client);
+        return DEL(key);
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
