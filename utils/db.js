70c9d55f483c74297679d8945b3406e08e5c4bf2
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-12-10 17:56:47 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,26 +1,25 @@
-// import mongodb from 'mongodb';
-
-import MongoClient from "mongodb/lib/mongo_client";
+import {MongoClient} from 'mongodb';
 
 class DBClient {
     constructor() {
-        this.host = process.env.HOST || 'localhost';
-        this.port = process.env.PORT || 27017;
-        this.database = process.env.DATABASE_NAME || 'files_manager';
+        this.host = process.env.DB_HOST || 'localhost';
+        this.port = process.env.DB_PORT || 27017;
+        this.database = process.env.DB_DATABASE || 'files_manager';
 
         this.init();
     }
 
-    init() {
-        return MongoClient.connect(`mongodb://${this.host}:${this.port}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((client) => {
-            this.client = client;
-            this.db = client.db(this.database);
-        }).catch((err) => {
+    async init() {
+        try {
+            this.client = await MongoClient.connect(`mongodb://${this.host}:${this.port}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+
+            this.db = this.client.db(this.database);
+        } catch (err) {
             console.error(err.message);
-        });
+        }
     }
 
     isAlive() {
@@ -28,12 +27,19 @@ class DBClient {
     }
 
     async nbUsers() {
-        return this.db.collection('users').countDocuments();
+        if (!this.db)
+            throw new Error('Database not initialized');
+
+        return await this.db.collection('users').countDocuments();
     }
 
-    async nbUsers() {
-        return this.db.collection('files').countDocuments();
+    async nbFiles() {
+        if (!this.db)
+            throw new Error('Database not initialized');
+
+        return await this.db.collection('files').countDocuments();
     }
 }
 
-export const dbClient = new DBClient();
+const dbClient = new DBClient();
+export default dbClient;
