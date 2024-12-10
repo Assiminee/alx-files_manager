3c3c395ec6562dca64b53b4297a1de723fd7252a
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-12-10 18:10:31 +0100

    API

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,42 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor() {
-        this.host = process.env.DB_HOST || 'localhost';
-        this.port = process.env.DB_PORT || 27017;
-        this.database = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
 
-        this.init();
+    this.init();
+  }
+
+  async init() {
+    try {
+      this.client = await MongoClient.connect(`mongodb://${this.host}:${this.port}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      this.db = this.client.db(this.database);
+    } catch (err) {
+      console.error(err.message);
     }
+  }
 
-    async init() {
-        try {
-            this.client = await MongoClient.connect(`mongodb://${this.host}:${this.port}`, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
+  isAlive() {
+    return !!this.db;
+  }
 
-            this.db = this.client.db(this.database);
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
+  async nbUsers() {
+    if (!this.db) { throw new Error('Database not initialized'); }
 
-    isAlive() {
-        return !!this.db;
-    }
+    return this.db.collection('users').countDocuments();
+  }
 
-    async nbUsers() {
-        if (!this.db)
-            throw new Error('Database not initialized');
+  async nbFiles() {
+    if (!this.db) { throw new Error('Database not initialized'); }
 
-        return await this.db.collection('users').countDocuments();
-    }
-
-    async nbFiles() {
-        if (!this.db)
-            throw new Error('Database not initialized');
-
-        return await this.db.collection('files').countDocuments();
-    }
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,33 @@
-import {createClient} from 'redis';
-import {promisify} from 'util';
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
+  constructor() {
+    this.client = createClient();
 
-        this.client.on('error', (err) => {
-            console.log(err.message);
-        });
-    }
+    this.client.on('error', (err) => {
+      console.log(err.message);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const GET = promisify(this.client.get).bind(this.client);
-        return GET(key);
-    }
+  async get(key) {
+    const GET = promisify(this.client.get).bind(this.client);
+    return GET(key);
+  }
 
-    async set(key, value, duration) {
-        const SET = promisify(this.client.set).bind(this.client);
-        return SET(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    const SET = promisify(this.client.set).bind(this.client);
+    return SET(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        const DEL = promisify(this.client.del).bind(this.client);
-        return DEL(key);
-    }
+  async del(key) {
+    const DEL = promisify(this.client.del).bind(this.client);
+    return DEL(key);
+  }
 }
 
 const redisClient = new RedisClient();
